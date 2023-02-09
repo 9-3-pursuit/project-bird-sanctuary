1db61b8b05b4583cb0fc1260d5baaaa7cc8ee6ad
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-09 16:47:17 -0500

    Adds static content for Checkout component

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,1 +1,38 @@
-export default function Checkout() {}
+export default function Checkout() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <form>
+        <label htmlFor="first-name">First Name</label>
+        <input
+          id="first-name"
+          name="first-name"
+          type="text"
+          placeholder="First name"
+        />
+        <label htmlFor="last-name">Last Name</label>
+        <input
+          id="last-name"
+          name="last-name"
+          type="text"
+          placeholder="Last name"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="YourEmail@email.com"
+        />
+        <label htmlFor="">Zip-Code</label>
+        <input
+          id="Zip-Code"
+          name="Zip-Code"
+          type="number"
+          placeholder="XXXXX"
+        />
+        <input type="submit"></input>
+      </form>
+    </div>
+  );
+}
