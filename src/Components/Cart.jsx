7a0314f4b49e4d653393ea6b306a3f05615f9073
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-09 16:30:21 -0500

    Adds static content to Cart component

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,1 +1,12 @@
-export default function Cart() {}
+export default function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+      <h2>Discount: %0</h2>
+      <h2>Total: $0</h2>
+      <ul></ul>
+      <p>Your donation has qualified you for the following bonuses</p>
+      <ol></ol>
+    </div>
+  );
+}
