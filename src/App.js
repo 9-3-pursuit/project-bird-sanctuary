9f80113c996d9ed5e5745301732bdf264c8b81e1
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-09 16:47:54 -0500

    Create page skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import birdData from "./data/birds";
+import Birdcard from "./Components/Birdcard";
+import Cart from "./Components/Cart";
+import Checkout from "./Components/Checkout";
+
 function App () {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <aside>
+        <Cart />
+        <Checkout />
+      </aside>
+      <Birdcard />
     </div>
   );
 };
---
+++ src/Components/Checkout.js
@@ -0,0 +1,8 @@
+
+export default function Checkout() {
+    return(
+        <form>
+            <h2>Checkout</h2>
+        </form>
+    )
+}
