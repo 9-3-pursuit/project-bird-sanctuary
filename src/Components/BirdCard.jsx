9a2eeba07fc845eaf323f299ca6dae13c53e0151
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-09 15:12:48 -0500

    Adds content to BirdCard

--- src/Components/BirdCard.jsx
+++ src/Components/BirdCard.jsx
@@ -1,1 +1,10 @@
-export default function BirdCard() {}
+export default function BirdCard({ name, img, amount, id }) {
+  return (
+    <div key={id}>
+      <h3>{name}</h3>
+      <p>{amount}</p>
+      <img src={img} alt={name} />
+      <button>Adopt</button>
+    </div>
+  );
+}
