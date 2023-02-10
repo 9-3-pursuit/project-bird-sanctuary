e006e48883adf7947ad671b194119a044e5ee6d3
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-10 13:08:43 -0500

    Style the page layout

--- src/Components/Birdcard.js
+++ src/Components/Birdcard.js
@@ -2,15 +2,21 @@
 
 export default function Birdcard({ birdData }) {
 
-    return birdData.map(bird => {
-        return(
-        <div className="card">
-            <p className="cardHeader">{bird.name}</p>
-            <p className="cardPrice">Price: ${bird.amount}</p>
-            <img className="cardImg" src={bird.img} alt={bird.name} />
-            <button className="cardButton">Adopt</button>
+    return(
+        <div className="cardsDiv">
+            {
+                birdData.map(bird => {
+                    return(
+                        <div className="card">
+                            <p className="cardHeader">{bird.name}</p>
+                            <p className="cardPrice">Price: ${bird.amount}</p>
+                            <img className="cardImg" src={bird.img} alt={bird.name} />
+                            <button className="cardButton">Adopt</button>
+                        </div>
+                    )
+                })  
+            }
         </div>
     )
-    })
     
 }
