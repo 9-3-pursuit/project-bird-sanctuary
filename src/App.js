7a924ea8e8118a353bdd577160172f5b2a986d9b
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-09 19:21:29 -0500

    Adds all components and styles to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App () {
+import BirdCard from "./Components/BirdCard";
+import Cart from "./Components/Cart";
+import Checkout from "./Components/Checkout";
+import "./App.css";
+import birdData from "./data/birds";
+
+function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <main>
+      <Cart />
+      <Checkout />
+      <div className="bird-section">
+        {birdData.map(({ name, img, amount, id }) => (
+          <BirdCard name={name} img={img} amount={amount} key={id} />
+        ))}
+      </div>
+    </main>
   );
-};
+}
 
 export default App;
