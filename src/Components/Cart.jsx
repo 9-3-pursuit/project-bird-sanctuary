b247eba4485513775d3b0adeb6592ebe4b4ea40e
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-09 21:16:35 -0500

    Adds state discount and applies discount to total

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,12 @@
 import "./Cart.css";
 
 export default function Cart({ list, total, setTotal }) {
+  const discount = list.length >= 3 ? 0.1 : 0;
   return (
     <section className={"Cart section border-black"}>
       <h4>Cart</h4>
-      <h5>Discount: 0%</h5>
-      <h5>Total: $ {total} </h5>
+      <h5>Discount: {discount * 100}%</h5>
+      <h5>Total: $ {total - total * discount} </h5>
       <ol>
         {list.map(({ name, amount, id }) => (
           <li id={id}>
