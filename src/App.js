dacd3120998c9acec743199c7ac22fab13f794f6
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-09 20:45:55 -0500

    Adds state and event handler functions

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import Cart from "./Components/Cart";
 import Checkout from "./Components/Checkout";
 import "./App.css";
 import birdData from "./data/birds";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("The form was submitted");
+  }
+
+  function handleAddToCart({ name, amount, id }) {
+    setList([...list, { name, amount, id }]);
+    setTotal(total + amount);
+  }
+
   return (
     <main>
       <Cart />
