d88ac5f66b1ec903cdadbc507932f8a78d2a98f3
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-02-10 12:07:49 -0500

    Got cards to display.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+// import React from "react";
+import  birdData  from "./data/birds";
+// import BirdCards from "./Components/Birds";
+import { useState } from "react";
+
+
 function App () {
+  const [birdsArray, getBirds]= useState(birdData);
+ 
+ 
+  function CreateBirdCards(){
+    const birdCards = birdsArray.map((bird) => (
+      <li key={bird.id} className="card">
+        <h4>{bird.name}</h4>
+        <img
+        src={bird.img}
+        width="200"
+        height="200"
+        alt="bird"
+        ></img>
+        <p>Total ${bird.amount}</p>
+        <button>
+          <h5>Adopt</h5>
+        </button>
+      </li>
+    ));
+  //  console.log(getBirds({...birdsArray}))
+
+    return birdCards
+    // return (
+    //   <ul>
+    //     <li>
+    //       {birdCards}
+    //     </li>
+       
+    //   </ul>
+    // )
+  }
+  
+    
   return (
     <div>
       <h1>Hello, world!</h1>
+      {/* <birdData/> */}
+      <CreateBirdCards />
     </div>
   );
 };
---
+++ src/Components/Birds.js
@@ -0,0 +1,17 @@
+// import React from "react"
+// import { useState } from "react";
+// import  birdData  from "./data/birds.js"
+export default function Birds() {
+   
+    // return (
+    //  <card className="card">
+    //     <h4 className="birdName">Bird Nmae</h4>
+    //     <img className="birdImage"
+    //     src=".//bird-sanctuary.gif"
+    //     alt="bird gif"></img>
+    //     <p>Price $120</p>
+    // </card>
+    // )
+   
+  
+ }
