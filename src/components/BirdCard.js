c21697aacb5378643f0e4de2514884891c357d43
HaroldF415 <harold.f415@gmail.com>
2023-02-11 15:23:54 -0500

    BirdCard component complete

--- src/components/BirdCard.js
+++ src/components/BirdCard.js
@@ -1,3 +1,13 @@
-const BirdCard = () => {};
+const BirdCard = ({ bird, handleAdoptClick }) => {
+  return (
+    <div className="card">
+      <strong>{bird.name}</strong>
+      <p>Price: ${bird.amount}</p>
+      <img src={bird.img} alt={bird.name} />
+      <br />
+      <button onClick={() => handleAdoptClick(bird.id)}>Adopt</button>
+    </div>
+  );
+};
 
 export default BirdCard;
