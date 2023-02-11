8442298f20bc68b799b5feadf9eb11e3cd7228ca
HaroldF415 <harold.f415@gmail.com>
2023-02-11 15:09:20 -0500

    resetCart, handleDeleteClick, handleAdoptClick functions completed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,37 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [discount, setDiscount] = useState(0);
+
+  const resetCart = () => {
+    setCart([]);
+    setTotal(0);
+    setDiscount(0);
+  };
+
+  const handleAdoptClick = (birdID) => {
+    const birdFoundInData = birdData.find((bird) => bird.id === birdID);
+    setTotal(total + birdFoundInData.amount);
+    setCart([...cart, birdFoundInData]);
+    if (cart.length > 2) {
+      setDiscount();
+    }
+  };
+
+  const handleDeleteClick = (birdID, birdIndex) => {
+    const birdFoundInCart = cart.find((bird) => bird.id === birdID);
+    const updatedCart = [...cart];
+
+    updatedCart.splice(birdIndex, 1);
+    setCart(updatedCart);
+    setTotal(total - birdFoundInCart.amount);
+    if (cart.length < 3) {
+      setDiscount(0);
+    }
+  };
+
   return (
     <div>
       <h1>Bird Sanctuary Project!</h1>
