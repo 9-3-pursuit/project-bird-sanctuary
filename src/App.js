79ae81287874e76bca917b18cfe54d227b780069
HaroldF415 <harold.f415@gmail.com>
2023-02-12 03:00:55 -0500

    THANK YOU ANGIE!!! learned useEffect()

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from "./components/Cart";
 import BirdCard from "./components/BirdCard";
 import Checkout from "./components/Checkout";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./styles/App.css";
 
@@ -13,6 +13,13 @@ const App = () => {
   const [total, setTotal] = useState(0);
   const [discount, setDiscount] = useState(0);
 
+  useEffect(() => {
+    console.log(cart.length);
+    if (cart.length >= 3) {
+      setDiscount(10);
+    }
+  }, [cart, discount]);
+
   const resetCart = () => {
     setCart([]);
     setTotal(0);
@@ -20,12 +27,13 @@ const App = () => {
   };
 
   const handleAdoptClick = (birdID) => {
+    console.log(cart.length);
     const birdFoundInData = birdData.find((bird) => bird.id === birdID);
     setTotal(total + birdFoundInData.amount);
     setCart([...cart, birdFoundInData]);
-    if (cart.length > 2) {
-      setDiscount(10);
-    }
+    // if (cart.length >= 3) {
+    //   setDiscount(10);
+    // }
   };
 
   const handleDeleteClick = (birdID, birdIndex) => {
