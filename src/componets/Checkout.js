08568930d932d73a808e0d0dddf0c3ac2b41369c
Keeanu Rodriguez <keeanu7@gmail.com>
2023-02-11 21:52:21 -0500

    css updated

--- src/componets/Checkout.js
+++ src/componets/Checkout.js
@@ -1,17 +1,23 @@
+import "./Checkout.css"
+
 function Checkout (props) {
 
     return (
-        <form className="Checkout" onSubmit={props.handleFormSubmit}>
-            <label htmlFor="firstName">First Name</label>
-            <input id="firstName" type="text"></input>
-            <label htmlFor="lastName">Last Name</label>
-            <input id="lastName" type="text"></input>
-            <label htmlFor="email">Email</label>
-            <input id="email" type="text"></input>
-            <label htmlFor="zipCode">Zip Code</label>
-            <input id="zipCode" type="number"></input>
-            <input type="submit"></input>
-        </form>
+        <div className="checkout">
+            <h3>Checkout</h3>
+            <form className="Checkout" onSubmit={props.handleFormSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input id="firstName" type="text"></input>
+                <label htmlFor="lastName">Last Name</label>
+                <input id="lastName" type="text"></input>
+                <label htmlFor="email">Email</label>
+                <input id="email" type="text"></input>
+                <label htmlFor="zipCode">Zip Code</label>
+                <input id="zipCode" type="number"></input>
+                <br></br>
+                <button className="button" type="submit">Submit</button>
+            </form>
+        </div>
     );
 }
 
