22c0077ab766bbe6a9d93af04ad7e7f9825a5054
HaroldF415 <harold.f415@gmail.com>
2023-02-14 09:49:34 -0500

    removed h1

--- src/components/BirdCard.js
+++ src/components/BirdCard.js
@@ -1,6 +1,8 @@
+import "../styles/BirdCard.css";
+
 const BirdCard = ({ bird, handleAdoptClick }) => {
   return (
-    <div className="card">
+    <div className="card birdCard">
       <strong>{bird.name}</strong>
       <p>Price: ${bird.amount}</p>
       <img src={bird.img} alt={bird.name} />
