0085b98a1e0582d647624f72faf6928f9d4d5134
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-02-14 15:51:00 -0500

    committing for after creating BirdCard,Cart and Checkout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from "react";
+import BirdCard from "./components/BirdCard"
+import Cart from "./components/Cart"
+import Checkout from "./components/Checkout"
+import { useState } from "react";
+
 function App () {
+  const [cart4Bird, setMyCart] = useState({total:0, discount:false, adopted:[]})
   return (
     <div>
-      <h1>Hello, world!</h1>
-    </div>
+       <main>
+        <div>
+          <div className="leftaside">
+          <Cart cart4Bird={cart4Bird} setMyCart={setMyCart}/>
+          </div>
+          <div>
+          <Checkout cart4Bird={cart4Bird} setMyCart={setMyCart}/>  
+         </div>
+         </div>
+         <BirdCard cart4Bird ={cart4Bird} setMyCart={setMyCart}/>
+         </main>
+         </div>
+   
   );
 };
 
---
+++ src/components/BirdCard.js
@@ -0,0 +1,47 @@
+import birdData from "../data/birds";
+
+
+
+function BirdCard({cart4Bird, setMyCart}){
+    function addToCart(bird)
+    {
+
+      let arr = cart4Bird.adopted
+      let theAdoptedBird = {birdname: bird.name,  birdamount: bird.amount}
+      arr.push(theAdoptedBird)
+      let temptotal = arr.map((x) => x.birdamount).reduce((a,b)=> a+b,0)
+      let myCart= {total: temptotal, adopted: arr }
+      
+      if(myCart.adopted.length >= 3) {
+     
+       let totalDisc = (temptotal * 0.9)
+       let adoptedBirds = myCart.adopted
+       let cart2 = {total: totalDisc , discount: true, adopted: adoptedBirds}
+       
+       setMyCart(cart2)
+      } 
+      else {
+      setMyCart(myCart)}
+
+    } 
+    return(
+        <div className="card">
+        <ul>
+          {birdData.map((bird, index) => {
+            return(
+            <li key={index} className="Card">
+            <h3>{bird.name}</h3> 
+            <img src={bird.img} alt=''/>
+            <p>Price {bird.amount}</p>
+            <button className="adoptButton" onClick={() => addToCart(bird)}>Adopt</button>   
+
+            </li>
+          
+            );
+          })}
+        </ul>
+      </div>
+
+);
+}
+    export default BirdCard;
