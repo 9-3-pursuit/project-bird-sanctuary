7690d67d49969cc37bd987f9328904b3fcc63ca9
Jyoti Singh <jyo.singh88@gmail.com>
2023-02-15 11:06:06 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-function App () {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
-};
+import React from 'react';
+import './App.css';
+import Birdcards from './Components/Birdcards';
+import birdData from './data/birds';
+import Cart from './Components/Cart';
+import Checkout from './Components/Checkout';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      savedBirds: [],
+    };
+  }
+
+  handleCartListener = (bird) => {
+    const addToTheCart = [...this.state.savedBirds, bird];
+    this.setState({
+      savedBirds: addToTheCart,
+    });
+  };
+  render() {
+    return (
+      <>
+        {birdData.map((bird) => {
+          return (
+            <Birdcards
+              handleCartListener={this.handleCartListener}
+              bird={bird}
+              className="birds"
+              key={bird.id}
+              name={bird.name}
+              amount={bird.amount}
+              img={bird.img}
+            />
+          );
+        })}
+        <Cart addBird={this.state.savedBirds} bonus={bonusItems} />
+        <Checkout className="Check-out" />
+      </>
+    );
+  }
+}
 
 export default App;
