978f13f18eba70401cbdc47a0932e8fc44d28b1e
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-02-15 10:17:46 -0500

    Add empty checkout component file

--- src/BirdInfo.js
+++ src/BirdInfo.js
@@ -1,9 +1,9 @@
 export default function BirdInfo(props) {
   return (
     <article>
-      <img src={props.img} alt={props.name} width="150" />
       <h5>{props.name}</h5>
       <p>Price: ${props.amount}</p>
+      <img src={props.img} alt={props.name} width="150" />
     </article>
   );
 }
--- src/Cart.js
+++ src/Cart.js
@@ -10,13 +10,20 @@ export default function Cart(props) {
     );
   });
   let total = 0;
+  let discount = 0;
   props.selected.forEach((item) => {
     total += item.amount;
   });
+  if (props.selected.length >= 3 && total > 100) {
+    discount = 10;
+    total *= 0.9;
+    console.log(total);
+  }
   return (
     <section className="Cart">
-      <h3>Cart</h3>
-      <h4>Discount: </h4>
+      <h2>Cart</h2>
+      <h3>Discount: {discount}%</h3>
+
       <h4>Total: ${total}</h4>
       <ol className="Cart">{cartList}</ol>
       <p>Your donations have qualified you for the following items:</p>
