78af871d9e3c3108fbf2b4c4da84adc7bc2ae90d
Karimah Reavis <Karimah586@gmail.com>
2023-02-15 05:21:27 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import data from "./birds.js";
+import birdList from "./BirdCard.js";
+
+
 function App () {
+  const [birds, setBirds ] = useState(data);
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <main>
+      <div className="App">
+        <birdList birds = {birds} />
+        <button onClick = {() => console.log('you clicked me')}>Adopt</button>
+      
+      </div>
+
+    </main>
+    
   );
 };
 
