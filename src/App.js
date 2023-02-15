97540da2d5a8c850be8eb411654f558a60f17ffa
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-15 13:24:58 -0500

    Restructures state to use reducer function

--- src/App.js
+++ src/App.js
@@ -4,38 +4,35 @@ import Cart from "./Components/Cart";
 import Checkout from "./Components/Checkout";
 /*         CSS, Data, State         */
 import birdData from "./data/birds";
-import { useState } from "react";
+import { useReducer } from "react";
+import { cartReducer, INITIAL_STATE } from "./cartReducer";
 import "./App.css";
 
 function App() {
-  const [cart, setCart] = useState({
-    list: [],
-    total: 0,
-  });
+  const [cart, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
   function handleSubmit(e) {
     e.preventDefault();
     alert("You have adopted birds. Thank you!");
-    setCart({
-      list: [],
-      total: 0,
-    });
+    dispatch({ type: "submitted" });
   }
 
-  function handleAddToCart({ name, amount, id }) {
-    const { total, list } = cart;
-    setCart({
-      list: [...list, { name, amount, id }],
-      total: total + amount,
+  function handleAddToCart(birdToAddToList) {
+    dispatch({
+      type: "added",
+      birdToAddToList: birdToAddToList,
+      amount: birdToAddToList.amount,
     });
   }
 
   function handleRemoveFromCart(e, amount) {
-    const { total, list } = cart;
-    let newList = list.filter(({ id }) => id !== e.target.parentNode.id);
-    setCart({
-      list: newList,
-      total: total - amount,
+    const listWithItemFiltered = cart.list.filter(
+      ({ id }) => id !== e.target.parentNode.id
+    );
+    dispatch({
+      type: "removed",
+      listWithItemFiltered: listWithItemFiltered,
+      amount: amount,
     });
   }
 
@@ -44,15 +41,8 @@ function App() {
       <Cart cart={cart} onClick={handleRemoveFromCart} />
       <Checkout onSubmit={handleSubmit} />
       <div className="bird-section">
-        {birdData.map(({ name, img, amount, id }) => (
-          <BirdCard
-            name={name}
-            img={img}
-            amount={amount}
-            id={id}
-            key={id}
-            onClick={handleAddToCart}
-          />
+        {birdData.map((bird) => (
+          <BirdCard key={bird.id} bird={bird} onClick={handleAddToCart} />
         ))}
       </div>
     </main>
--- src/Components/BirdCard.jsx
+++ src/Components/BirdCard.jsx
@@ -1,7 +1,7 @@
 import "./BirdCard.css";
 import { v4 as uuidv4 } from "uuid";
 
-export default function BirdCard({ name, img, amount, id, onClick }) {
+export default function BirdCard({ bird: { name, img, amount, id }, onClick }) {
   return (
     <div key={id} className="card birds border-black">
       <h5>{name}</h5>
