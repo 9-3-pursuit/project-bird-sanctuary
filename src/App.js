08958424fde73beeca204bf8b42739003aa851a4
wal3sca <walescaambroise@gmail.com>
2023-02-22 23:50:53 -0500

    added bird cards array to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import BirdCard from "./components/BirdCard"
+import Cart from "./components/Cart"
+import Checkout from "./component/Checkout"
+import Birds from "./data/Birds"
+import "./App.css"
+
 function App () {
+  const birdCards = [
+    <li>
+    <BirdCard bird={birds[0]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[1]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[2]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[3]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[4]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[5]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[6]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[7]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[8]} />
+    </li>,
+    <li>
+    <BirdCard bird={birds[9]} />
+    </li>
+    
+  ]
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="App">
+    <Cart />
+    <Checkout />
+    <h1>Welcome to the Bird Sanctuary</h1>
+    <ul className="Birds">
+    {birdCards}
+    
+    </ul>
     </div>
-  );
-};
+    );
+  };
 
 export default App;
